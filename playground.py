2aa28a32ec748a53b2ed578ef4afccf31c16a33e
HPLegion <32222819+HPLegion@users.noreply.github.com>
2018-06-15 10:25:13 +0200

    testting of CsLMM

--- playground.py
+++ playground.py
@@ -8,9 +8,9 @@ import factools.dr
 
 
 # Set up script
-AI_FILE = "example_data/K.b-kll.ai"
-TR_FILE = "example_data/K.b-kll.tr"
-LEV_FILE = "example_data/K.b-kll.lev"
+AI_FILE = "cs/Cs_LMM-Li.ai"
+TR_FILE = "cs/Cs_LMM-Li.tr"
+LEV_FILE = "cs/Cs_LMM-Li.lev"
 
 # Read FAC Files
 ai_header, ai_df = factools.fileimport.read_ai(AI_FILE)
@@ -21,9 +21,9 @@ lev_header, lev_df = factools.fileimport.read_lev(LEV_FILE)
 # print(tr_df.head())
 # print(ai_df.head())
 
-lev_df = factools.reconstruction.amend_level_dataframe(lev_df, verbose=False)
+lev_df = factools.reconstruction.amend_level_dataframe(lev_df, verbose=True)
 
-trans = factools.dr.dr_transition_table(lev_df, ai_df, tr_df, verbose=False)
-recomb = factools.dr.dr_recombination_table(lev_df, ai_df, tr_df, verbose=False)
+trans = factools.dr.dr_transition_table(lev_df, ai_df, tr_df, verbose=True)
+recomb = factools.dr.dr_recombination_table(lev_df, ai_df, tr_df, verbose=True)
 print(trans.head())
 print(recomb)
